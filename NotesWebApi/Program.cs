5a96c1e2aba1612dfc2badbfe2af40e6357a5dd5
German Belykh <gbelyh@kodeks.ru>
2022-03-30 12:12:11 +0300

    *Add DI for Persistence and Application *Add AutoMapper *Set cors policy *Add Controllers and Models

--- NotesWebApi/Program.cs
+++ NotesWebApi/Program.cs
@@ -1,16 +1,26 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using NotesPersistence;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace NotesWebApi {
     public class Program {
         public static void Main ( string[] args ) {
-            CreateHostBuilder ( args ).Build ().Run ();
+            var host = CreateHostBuilder ( args ).Build ();
+
+            using ( var scope = host.Services.CreateScope () ) {
+                var serviceProvider = scope.ServiceProvider;
+                try {
+                    var context = serviceProvider.GetRequiredService<NotesDbContext> ();
+                    DbInitializer.Initialize ( context );
+                }
+                catch ( Exception exception ) {
+
+                }
+            }
+
+            host.Run ();
         }
 
         public static IHostBuilder CreateHostBuilder ( string[] args ) =>
--- NotesWebApi/Startup.cs
+++ NotesWebApi/Startup.cs
@@ -1,32 +1,53 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using NotesApplication;
+using NotesApplication.Common.Mappings;
+using NotesApplication.Interfaces;
+using NotesPersistence;
+using System.Reflection;
 
 namespace NotesWebApi {
     public class Startup {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices ( IServiceCollection services ) {
+
+        public IConfiguration Configuration { get; set; }
+
+        public Startup ( IConfiguration configuration ) {
+            Configuration = configuration;
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void ConfigureServices ( IServiceCollection services ) {
+            services.AddAutoMapper ( config => {
+                config.AddProfile ( new AssemblyMappingProfile ( Assembly.GetExecutingAssembly () ) );
+                config.AddProfile ( new AssemblyMappingProfile ( typeof ( INotesDbContext ).Assembly ) );
+            } );
+
+            services.AddApplication ();
+            services.AddPersistence ( Configuration );
+            services.AddControllers ();
+
+            services.AddCors ( options => {
+                options.AddPolicy ( "AllowAll" , policy => {
+                    policy.AllowAnyHeader ();
+                    policy.AllowAnyMethod ();
+                    policy.AllowAnyOrigin ();
+                } );
+            } );
+        }
+
         public void Configure ( IApplicationBuilder app , IWebHostEnvironment env ) {
             if ( env.IsDevelopment () ) {
                 app.UseDeveloperExceptionPage ();
             }
 
             app.UseRouting ();
+            app.UseHttpsRedirection ();
+            app.UseCors ( "AllowAll" );
 
             app.UseEndpoints ( endpoints => {
-                endpoints.MapGet ( "/" , async context => {
-                    await context.Response.WriteAsync ( "Hello World!" );
-                } );
+                endpoints.MapControllers ();
             } );
         }
     }
